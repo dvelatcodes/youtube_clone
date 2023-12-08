8905338b75d0c9a4a1cd04bb319a14492664e6db
Love Olubummo <olubummolove3@gmail.com>
2023-12-08 16:05:56 +0100

    updated video card

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -16,14 +16,47 @@ const VideoCard = ({
   },
 }) => {
   return (
-    <Card>
+    <Card
+      sx={{
+        width: { md: '320px', xs: '100%' },
+        borderRadius: 0,
+        boxShadow: 'none',
+      }}
+    >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
-          sx={{ height: 358, width: 180 }}
+          sx={{ height: 180, width: 358 }}
         />
       </Link>
+      <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography
+            variant='subtitle1'
+            fontWeight='bold'
+            color='#fff'
+          >
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography
+            variant='subtitle2'
+            fontWeight='bold'
+            color='gray'
+          >
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle sx={{ color: 'gray', fontSize: 12, ml: '5px' }} />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   );
 };
